0a103a38e6dace2538cedfcca32c29eb4094d1c8
evgeniy-kulikov <redstepway@gmail.com>
2024-10-07 21:49:32 +0300

    start make activity

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/linearLayoutLearn.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class linearLayoutLearn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_learn)
+    }
+}
