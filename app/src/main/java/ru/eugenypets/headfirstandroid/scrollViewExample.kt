c6f791de7123bfbb0dd67e0526da0d73e064bb5f
evgeniy-kulikov <redstepway@gmail.com>
2024-10-09 17:54:39 +0300

    ScrollView

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/scrollViewExample.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class scrollViewExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_view_example)
+    }
+}
