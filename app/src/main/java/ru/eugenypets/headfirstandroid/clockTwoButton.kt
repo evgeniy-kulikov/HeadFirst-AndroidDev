83433cf3815452946c3debb31f389eda2c2813f8
evgeniy-kulikov <redstepway@gmail.com>
2024-10-09 20:21:40 +0300

    Clock Two Button

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/clockTwoButton.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class clockTwoButton : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clock_two_button)
+    }
+}
