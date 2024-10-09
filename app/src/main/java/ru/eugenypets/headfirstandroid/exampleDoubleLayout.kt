b08f274dd7e4630a269c02bf37fbd99f4d01a6c5
evgeniy-kulikov <redstepway@gmail.com>
2024-10-09 21:19:01 +0300

    example Double Layout

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/exampleDoubleLayout.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class exampleDoubleLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_double_layout)
+    }
+}
