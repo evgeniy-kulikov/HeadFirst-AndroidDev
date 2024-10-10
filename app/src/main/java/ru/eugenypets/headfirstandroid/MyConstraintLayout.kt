aa5896e8c6bd7bf12d52e6f8f79a9bd95497c65f
evgeniy-kulikov <redstepway@gmail.com>
2024-10-10 13:00:33 +0300

    edit & radius button

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/MyConstraintLayout.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_constraint_layout)
+    }
+}
