f325d68bf2b437c0e1830b948371c1fda2191012
evgeniy-kulikov <redstepway@gmail.com>
2024-10-11 22:11:28 +0300

    two groups from of three buttons with chain

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/ThreeButton.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreeButton : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three_button)
+    }
+}
