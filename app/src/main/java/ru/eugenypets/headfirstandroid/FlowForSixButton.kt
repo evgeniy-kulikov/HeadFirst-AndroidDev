0ac7047e9755aa8ba1aae2cdc28b345948c7107b
evgeniy-kulikov <redstepway@gmail.com>
2024-10-13 12:00:12 +0300

    Flow from groups of six buttons

---
+++ app/src/main/java/ru/eugenypets/headfirstandroid/FlowForSixButton.kt
@@ -0,0 +1,11 @@
+package ru.eugenypets.headfirstandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlowForSixButton : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow_for_six_button)
+    }
+}
